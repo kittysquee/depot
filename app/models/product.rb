375e214ef3694af301ccb0b1095286f14482bed9
Elaine Osbourn <kittysquee@gmail.com>
2017-09-08 16:08:53 +0100

    Adds validations to products model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+
+  validates :title, uniqueness: true, presence: true
+  validates :description, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}, presence: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif\jpg\png\Z)}i,
+    message: 'must be a URL for gif, jpg or png image.'
+  }
 end
